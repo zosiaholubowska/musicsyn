ddfefaccfd18e0f5d4c7cf15cacdf5079f8d8821
pfriedrich-hub <lukas.lange01@gmail.com>
2024-03-20 11:38:11 +0100

    data collection

--- experiment/pilot_musicsyn_eeg.py
+++ experiment/pilot_musicsyn_eeg.py
@@ -120,7 +120,7 @@ def run(melody_file, subject, p, condition):
 
 def select_file():
     subjects = [f for f in os.listdir(f"{path}/Results")]
-    participant = "sub15_eeg"  ########### PARTICIPANT HERE ############
+    participant = "sub14_eeg"  ########### PARTICIPANT HERE ############
 
     if participant in subjects:
         print(subjects)
